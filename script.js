f0a6fd9330e8d9835f2b2462e130cbbb5a665a37
Abdullahalbakhit <abdullahalbakhit5@gmail.com>
2022-08-18 18:56:36 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function bmi(weght,height) {
+    let result = weight / (height*height)
+    return result
+}
 
+console.log(bmi(10,10))
+
+function Status (bmi) {
+    if (bmi < 18.5) {
+        return `لديك نقص في الوزن`
+} else if (bmi >= 18.5 && bmi < 25) {
+    return `وزنك صحي`
+}else if (bmi > 25) {
+    return `لديك زياده في الوزن`
+}
+}
+
+function calculate() {
+    let weight = document.getElementById(`weight`).value
+    let height = document.getElementById(`height`).value
+
+    let bmi = BMI(weight,height)
+    let desc = Status(bmi)
+
+    let div = document.getElementById(`divTag`)
+
+    div.innerText = bmi + " == " + desc
+}
